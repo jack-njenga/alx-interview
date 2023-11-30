0009db2bdb43f6da23e8294597f525c670b13785
jack-njenga <jacknjenga99@gmail.com>
2023-11-30 13:14:59 +0300

    .

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,6 +3,7 @@
 Returns a list of lists of integers representing the Pascal's triangle of n
 """
 
+
 def pascal_triangle(n):
     """
     Returns an empty list if n <= 0
@@ -21,6 +22,6 @@ def pascal_triangle(n):
                     new_lst.append(lst[-1][ii] + lst[-1][(ii - 1)])
                 new_lst.append(1)
                 lst.append(new_lst)
-                
+
         return lst
 
